cecca4fddedaa2bc66f1e3015ac4f961e2e23385
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-10-24 17:52:23 -0500

    Add result box styles

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 
 export default class Calculator extends React.PureComponent {
   render() {
-    return (<h1>Hello World!</h1>);
+    return (
+
+      <section className="calculator-container">
+        <div className="calculator">
+          <div className="gray-box">
+            <p>12345670</p>
+          </div>
+          <div className="operation-grid" />
+        </div>
+      </section>
+
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles.css';
 import Calculator from './components/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
