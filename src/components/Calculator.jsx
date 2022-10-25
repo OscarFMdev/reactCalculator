9ca62289417115203e68a89d5ea752453e19f88f
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-10-24 22:35:21 -0500

    Mobile version styles

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -9,25 +9,25 @@ export default class Calculator extends React.PureComponent {
             <p>12345670</p>
           </div>
           <ul className="operation-grid">
+            <li className="gray-button">AC</li>
+            <li className="gray-button">+/-</li>
+            <li className="gray-button">%</li>
+            <li className="orange-button">÷</li>
+            <li className="gray-button">7</li>
+            <li className="gray-button">8</li>
+            <li className="gray-button">9</li>
+            <li className="orange-button">X</li>
+            <li className="gray-button">4</li>
+            <li className="gray-button">5</li>
+            <li className="gray-button">6</li>
+            <li className="orange-button">-</li>
             <li className="gray-button">1</li>
-            <li className="gray-button">1</li>
-            <li className="gray-button">1</li>
-            <li className="orange-button">1</li>
-            <li className="gray-button">1</li>
-            <li className="gray-button">1</li>
-            <li className="gray-button">1</li>
-            <li className="orange-button">1</li>
-            <li className="gray-button">1</li>
-            <li className="gray-button">1</li>
-            <li className="gray-button">1</li>
-            <li className="orange-button">1</li>
-            <li className="gray-button">1</li>
-            <li className="gray-button">1</li>
-            <li className="gray-button">1</li>
-            <li className="orange-button">1</li>
-            <li className="gray-button large">1</li>
-            <li className="gray-button">1</li>
-            <li className="orange-button">1</li>
+            <li className="gray-button">2</li>
+            <li className="gray-button">3</li>
+            <li className="orange-button">+</li>
+            <li className="gray-button large">0</li>
+            <li className="gray-button">.</li>
+            <li className="orange-button">=</li>
           </ul>
         </div>
       </section>
