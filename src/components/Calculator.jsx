bf006dd92e11fce42a9bbbf1e511b14f58b8e00f
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-10-24 22:25:00 -0500

    Add calculator styles

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,13 +3,32 @@ import React from 'react';
 export default class Calculator extends React.PureComponent {
   render() {
     return (
-
       <section className="calculator-container">
         <div className="calculator">
           <div className="gray-box">
             <p>12345670</p>
           </div>
-          <div className="operation-grid" />
+          <ul className="operation-grid">
+            <li className="gray-button">1</li>
+            <li className="gray-button">1</li>
+            <li className="gray-button">1</li>
+            <li className="orange-button">1</li>
+            <li className="gray-button">1</li>
+            <li className="gray-button">1</li>
+            <li className="gray-button">1</li>
+            <li className="orange-button">1</li>
+            <li className="gray-button">1</li>
+            <li className="gray-button">1</li>
+            <li className="gray-button">1</li>
+            <li className="orange-button">1</li>
+            <li className="gray-button">1</li>
+            <li className="gray-button">1</li>
+            <li className="gray-button">1</li>
+            <li className="orange-button">1</li>
+            <li className="gray-button large">1</li>
+            <li className="gray-button">1</li>
+            <li className="orange-button">1</li>
+          </ul>
         </div>
       </section>
 
