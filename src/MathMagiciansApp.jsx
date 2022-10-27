babe38918656167a9b8eb38a0538d0fb64dc8e09
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-10-26 21:26:58 -0500

    Change MathMagiciansApp to functional component

--- src/MathMagiciansApp.jsx
+++ src/MathMagiciansApp.jsx
@@ -1,10 +1,5 @@
-import { PureComponent } from 'react';
 import Calculator from './components/Calculator';
 
-export default class MathMagiciansApp extends PureComponent {
-  render() {
-    return (
-      <Calculator />
-    );
-  }
-}
+const MathMagiciansApp = () => <Calculator />;
+
+export default MathMagiciansApp;
