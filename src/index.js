8bba0efaa717544f67afe363659e61ebe93f3fde
OscarFMdev <oscarfernandezm95@gmail.com>
2022-11-01 16:34:30 -0600

    Rename files

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 import { BrowserRouter } from 'react-router-dom';
-import MathMagiciansApp from './MathMagiciansApp';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <MathMagiciansApp />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
