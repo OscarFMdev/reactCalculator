02be812e3b0b021396c7a2bd7afce8b7c3648ce5
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-11-02 19:47:19 -0600

    Add Calculator tests

--- src/tests/components/Calculator.test.js
+++ src/tests/components/Calculator.test.js
@@ -1,1 +1,25 @@
-// import Calculator from "../../components/Calculator";
+import { fireEvent, render } from '@testing-library/react';
+import Calculator from '../../components/Calculator';
+
+describe('Calculator', () => {
+  /* RENDER */
+  test('should render', () => {
+    const calculator = render(<Calculator />);
+    expect(calculator).toMatchSnapshot();
+  });
+
+  /* USER INTERACTION */
+  test('should modify the result-box content after user pressing the buttons "7"', () => {
+    /* Arrange */
+    const { container } = render(<Calculator />);
+    const buttons = container.querySelectorAll('button');
+    const buttonSeven = buttons[4];
+
+    /* Act */
+    fireEvent.click(buttonSeven);
+    const resultBox = container.querySelector('.result-box > p').innerHTML;
+
+    /* Assert */
+    expect(resultBox).toBe('7');
+  });
+});
