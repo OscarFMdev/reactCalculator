35b680e7215135f470c1214d6cdca177642ce057
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-11-02 22:13:08 -0600

    Add calculate tests

--- src/tests/logic/calculate.test.js
+++ src/tests/logic/calculate.test.js
@@ -20,6 +20,57 @@ describe('calculate', () => {
     expect(result.total).toBe('12');
   });
 
+  test('AC should return 0', () => {
+    /* Arrange */
+    const total = 7;
+    const next = 5;
+    const operation = null;
+
+    /* Act */
+    const result = calculate({
+      total,
+      next,
+      operation,
+    }, '=');
+
+    /* Assert */
+    expect(result.total).toBe(0);
+  });
+
+  test('7 . 5 should be 7.5', () => {
+    /* Arrange */
+    const total = '7';
+    const next = '5';
+    const operation = null;
+
+    /* Act */
+    const result = calculate({
+      total,
+      next,
+      operation,
+    }, '.');
+
+    /* Assert */
+    expect(result.total).not.toEqual('7.5');
+  });
+
+  test('7 +/- 5 should be -7', () => {
+    /* Arrange */
+    const total = -'7';
+    const next = null;
+    const operation = null;
+
+    /* Act */
+    const result = calculate({
+      total,
+      next,
+      operation,
+    }, '+/-');
+
+    /* Assert */
+    expect(result.total).toBe('7');
+  });
+
   /* RENDERING TESTS */
   test('should create a div element containing the result', () => {
     /* Arrange */
